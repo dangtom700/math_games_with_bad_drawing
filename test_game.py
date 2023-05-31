b22941df69f95e249a1b58e98e87f70c87d834dc
Azalea1047 <azalea1047@gmail.com>
2023-05-31 22:59:27 +0000

    complete tax collector game

--- test_game.py
+++ test_game.py
@@ -32,23 +32,30 @@ def tax_collector_turn(remaining_number_list: list, player_number: int):
 
     return total_collect
 
-ceiling_number = 15
+ceiling_number = 100
 iterate_list = lambda n: [i for i in range(1, n+1)]
 num_list = iterate_list(ceiling_number)
 
 player_sum = 0
 tax_collector_sum = 0
 Is_continue_game = True
-back_track_index = -1
+back_track_index = 1
 
 # game loop here
 while Is_continue_game == True:
-    choosen_number = num_list[len(num_list)+back_track_index]
+    if len(num_list) < 2:
+        Is_continue_game = False
+        break
+
+    choosen_number = num_list[len(num_list) - back_track_index]
 
     Is_tax_collector_happy = forecast_tax_collector(num_list, choosen_number)
     while Is_tax_collector_happy == False:
-        back_track_index -= 1
-        choosen_number = num_list[len(num_list)+back_track_index]
+        if back_track_index >= len(num_list) - 1:
+            Is_continue_game = False
+            break
+        back_track_index += 1
+        choosen_number = num_list[len(num_list) - back_track_index]
         Is_tax_collector_happy = forecast_tax_collector(num_list, choosen_number)
 
     player_sum += choosen_number
